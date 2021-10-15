05aa99b35a90c0f51f202b31461558cdd6a4ed00
alexanderlio <88853383+alexanderlio@users.noreply.github.com>
2021-10-15 10:46:20 -0700

    5th commit

--- Read.java
+++ Read.java
@@ -1,8 +1,6 @@
 public class Read{
     public static void main(String[] args){
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
+        System.out.println("hi");
 
     }
 }
